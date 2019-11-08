5262e2aadf93b72bd8aa70cc052115081aa47e92
Edward Gavin <wsw_it_3325@163.com>
2019-11-08 14:25:07 +0800

    add interface question

--- src/main/java/com/xiaokea/demo/entity/QuestionResponse.java
+++ src/main/java/com/xiaokea/demo/entity/QuestionResponse.java
@@ -12,6 +12,55 @@ public class QuestionResponse {
     private String type;
     private List<String> options;
     private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<String> details) {
+        this.details = details;
+    }
+
     private List<String> details;
 
 }
